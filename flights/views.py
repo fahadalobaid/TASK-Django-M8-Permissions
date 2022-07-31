9aa4e8476c3a332da595313c03e2680a9cd89569
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-07-31 20:00:00 +0300

    still didn't finish step num 6

--- flights/views.py
+++ flights/views.py
@@ -4,6 +4,8 @@ from rest_framework import generics
 
 from flights import serializers
 from flights.models import Booking, Flight
+from rest_framework.permissions import  IsAuthenticated, IsAdminUser
+from rest_framework.permissions import BasePermission
 
 
 class FlightsList(generics.ListAPIView):
@@ -13,6 +15,7 @@ class FlightsList(generics.ListAPIView):
 
 class BookingsList(generics.ListAPIView):
     serializer_class = serializers.BookingSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         today = datetime.date.today()
@@ -24,13 +27,14 @@ class BookingDetails(generics.RetrieveAPIView):
     serializer_class = serializers.BookingDetailsSerializer
     lookup_field = "id"
     lookup_url_kwarg = "booking_id"
-
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 class UpdateBooking(generics.RetrieveUpdateAPIView):
     queryset = Booking.objects.all()
     lookup_field = "id"
     lookup_url_kwarg = "booking_id"
-
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
     def get_serializer_class(self):
         if self.request.user.is_staff:
             return serializers.AdminUpdateBookingSerializer
@@ -38,15 +42,19 @@ class UpdateBooking(generics.RetrieveUpdateAPIView):
         return serializers.UpdateBookingSerializer
 
 
-class CancelBooking(generics.DestroyAPIView):
+class CancelBooking(generics.DestroyAPIView,BasePermission):
     queryset = Booking.objects.all()
     lookup_field = "id"
     lookup_url_kwarg = "booking_id"
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    message = "You must be the author of this article."
 
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff or obj.author == request.user
 
 class BookFlight(generics.CreateAPIView):
     serializer_class = serializers.AdminUpdateBookingSerializer
-
+    permission_classes = [IsAuthenticated]
     def perform_create(self, serializer):
         serializer.save(user=self.request.user, flight_id=self.kwargs["flight_id"])
 
